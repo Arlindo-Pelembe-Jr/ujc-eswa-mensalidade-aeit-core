f51002c6cd75c8bb923757d9299155495d1682c3
Arlindo-Pelembe-Jr <aapelembejunior@gmail.com>
2022-04-01 14:25:51 +0200

    use autowired for instance primary in case on many beans

--- src/main/java/com/ujc/eswa/mensalidade/aeit/service/Service.java
+++ src/main/java/com/ujc/eswa/mensalidade/aeit/service/Service.java
@@ -1,5 +1,6 @@
 package com.ujc.eswa.mensalidade.aeit.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.ujc.eswa.mensalidade.aeit.model.Estudante;
@@ -7,7 +8,7 @@ import com.ujc.eswa.mensalidade.aeit.notification.Notification;
 
 @Component
 public class Service {
-	
+	@Autowired(required=false)
 	private Notification notify;
 	
 	public Service(Notification notify) {
