1f932303c571e3115a3506e5dc2fa2ad1549da7a
Arlindo-Pelembe-Jr <aapelembejunior@gmail.com>
2022-04-12 14:31:19 +0200

    adding cross origin to fix error when retrieve data

--- src/main/java/com/ujc/eswa/mensalidade/aeit/controller/EstudanteController.java
+++ src/main/java/com/ujc/eswa/mensalidade/aeit/controller/EstudanteController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,8 @@ import com.ujc.eswa.mensalidade.aeit.repository.EstudanteRepository;
 
 @RestController
 @RequestMapping(value="ujc-mensalidade/api/v1/estudantes")
+@CrossOrigin
+
 public class EstudanteController {
 	@Autowired
 	private EstudanteRepository estudanteRepository;
