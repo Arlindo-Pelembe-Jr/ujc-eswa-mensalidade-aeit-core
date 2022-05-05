ead6f54aa39be18313fee6ca43c01ae90b556cd5
Arlindo-Pelembe-Jr <aapelembejunior@gmail.com>
2022-05-05 10:34:18 +0200

    necessary sql for start up the project

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,16 +5,16 @@ INTO departamento (nome_departamento)
 VALUES ('FCT'); 
 
 INSERT 
-INTO curso (nome_curso,id_departamento)
-VALUES ('ETSI',1);
+INTO curso (nome_curso)
+VALUES ('ETSI');
 
 INSERT 
-INTO curso (nome_curso,id_departamento)
-VALUES ('AP',1),('RI',1);
+INTO curso (nome_curso)
+VALUES ('AP'),('RI');
 
 INSERT 
-INTO estudante (nome,curso_id_fk) 
-VALUES('Edwina',1),('Teofilo',1),('Israel',1);
+INTO estudante (nome,cod_estudante,curso_curso_codigo) 
+VALUES('Edwina',100,1),('Teofilo',101,1),('Israel',102,1);
 -- UPDATE 
 -- curso 
 -- SET id_departamento = 1
@@ -24,4 +24,4 @@ insert into funcionario (data_ingresso_funcionario) values('2020-04-02');
 
 insert into disciplina (disc_credito,disc_nome,estatuto) values (4,'SAG',' ');
 
-insert into inscricao (id,cod_inscricao,estudante_id,id_disciplina) values(1,100,1,1);
+insert into inscricao (cod_inscricao,estudante_id,disciplina_id) values(1,1,1);
