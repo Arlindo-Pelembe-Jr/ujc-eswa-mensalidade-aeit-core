98378fd0c26258ebb1d43ca5fba30cc1262df468
Arlindo-Pelembe-Jr <aapelembejunior@gmail.com>
2022-06-11 00:36:39 +0200

    comment unused app exception handler and create get mapping for classes by students

--- src/main/java/com/ujc/eswa/mensalidade/aeit/controller/IncricaoController.java
+++ src/main/java/com/ujc/eswa/mensalidade/aeit/controller/IncricaoController.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ujc.eswa.mensalidade.aeit.model.Estudante;
 import com.ujc.eswa.mensalidade.aeit.model.Inscricao;
+import com.ujc.eswa.mensalidade.aeit.repository.EstudanteRepository;
 import com.ujc.eswa.mensalidade.aeit.repository.InscricaoRepository;
 
 @RestController
@@ -19,9 +22,20 @@ public class IncricaoController {
 	
 	@Autowired
 	private InscricaoRepository inscricaoRepository;
-	
+	@Autowired 
+	private EstudanteRepository estudanteRepository;
 	@GetMapping
 	public ResponseEntity<List<Inscricao>> getAll(){
+		List<Inscricao> inscricoesInscricaos = inscricaoRepository.findAll();
+		if(!inscricoesInscricaos.isEmpty()) {
+			return ResponseEntity.ok().body(inscricoesInscricaos);
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@GetMapping("/bystudent")
+	public ResponseEntity<List<Inscricao>> getByStudent(@PathVariable Long utilizadorId){
+		Estudante estudante = estudanteRepository.findEstudanteByUtilizadorId(utilizadorId);
 		List<Inscricao> inscricoesInscricaos = inscricaoRepository.findAll();
 		if(!inscricoesInscricaos.isEmpty()) {
 			return ResponseEntity.ok().body(inscricoesInscricaos);
